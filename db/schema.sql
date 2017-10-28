2feafb9e2f396c83ec0ab9a2bed45a6bff7ed083
wchau <wchau8899@gmail.com>
2017-10-28 17:42:23 -0600

    change schema constraint

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ DROP  database IF EXISTS burgers_db;
 CREATE database burgers_db;
 use burgers_db;
 CREATE table burgers (
-	id INTEGER AUTO_INCREMENT,
-	burger_name VARCHAR(20),
+	id INTEGER AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(40),
 	devoured BOOLEAN,
-	date TIMESTAMP,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
